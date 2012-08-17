e89045134a73b56c5c80b4238ff8f82a44182e5b
Jeremy Foo <jeremyfoo@gmail.com>
2012-08-18 03:26:01 +0800

    Initial Commit

---
+++ EdventureKit/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  EdventureKit
+//
+//  Created by Jeremy Foo on 18/8/12.
+//  Copyright (c) 2012 Jeremy Foo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
